b96a1c28e315489da2e31f4d5d0f76980e4b87c0
Waalan <kennydanieldiaz@gmail.com>
2026-03-16 17:00:09 -0500

    Eliminar la definición de la función componentesVelocidad en calcularProyectil.hs

--- calcularProyectil.hs
+++ calcularProyectil.hs
@@ -1,13 +1,3 @@
-module CalcularProyectil
-  ( componentesVelocidad
-  )
-where
+import Funciones (coseno, radianesAGrados, gradosARadianes, potencia, seno)
 
-import Funciones (coseno, gradosARadianes, seno)
-
--- Ejemplo de uso de las funciones exportadas desde Funciones:
--- Dada una velocidad inicial y un ángulo en grados, retorna (vx, vy).
-componentesVelocidad :: Float -> Float -> (Float, Float)
-componentesVelocidad velocidad anguloGrados =
-  let anguloRad = gradosARadianes anguloGrados
-   in (velocidad * coseno anguloRad, velocidad * seno anguloRad)
+--
